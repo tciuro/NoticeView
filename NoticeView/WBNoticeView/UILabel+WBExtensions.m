dbcd9099ba5212bfcb71c03b7d05aa48d9d28c6f
Matthew Strickland <matthew@idlefusion.com>
2013-04-13 15:11:10 -0300

    Fix deprecation warning for LineBreak

--- NoticeView/WBNoticeView/UILabel+WBExtensions.m
+++ NoticeView/WBNoticeView/UILabel+WBExtensions.m
@@ -37,7 +37,7 @@
         
         NSRange rTest = NSMakeRange(rRemainingText.location, rWhitespace.location - rRemainingText.location);
         NSString *textTest = [self.text substringWithRange:rTest];
-        CGSize sizeTest = [textTest sizeWithFont:self.font forWidth:1024.0 lineBreakMode:UILineBreakModeWordWrap];
+        CGSize sizeTest = [textTest sizeWithFont:self.font forWidth:1024.0 lineBreakMode:NSLineBreakByWordWrapping];
         
         if (sizeTest.width > self.bounds.size.width) {
             [lines addObject:[currentLine stringByTrimmingCharactersInSet:wordSeparators]];
